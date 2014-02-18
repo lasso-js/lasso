a53f792b65b1dc5a53d48108a655a2b3cd7db9fd
Patrick Steele-Idem <psteeleidem@ebay.com>
2014-02-18 08:43:36 -0700

    Fixed test case

--- test/resources/foo-bar-bundle.js
+++ test/resources/foo-bar-bundle.js
@@ -6,7 +6,7 @@ $rmod.def("/baz@3.0.0/lib/index", function(require, exports, module, __filename,
 $rmod.def("/foo@1.0.0/lib/index", function(require, exports, module, __filename, __dirname) { exports.foo = "1.0.0";
 require('baz'); });
 $rmod.dep("", "bar", "2.0.0");
-$rmod.main("/bar@2.0.0", "lib/index");
+$rmod.main("/bar@2.0.0", "lib/bar");
 $rmod.dep("/$/bar", "baz", "3.0.0");
-$rmod.def("/bar@2.0.0/lib/index", function(require, exports, module, __filename, __dirname) { exports.bar = "2.0.0";
+$rmod.def("/bar@2.0.0/lib/bar", function(require, exports, module, __filename, __dirname) { exports.bar = "2.0.0";
 require('baz'); });
