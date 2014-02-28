178ea1c51d38268c37ff82430d74c8199e8b8017
Patrick Steele-Idem <psteeleidem@ebay.com>
2014-02-27 17:31:27 -0700

    Fixed "img" tag

--- taglib/img-tag.js
+++ taglib/img-tag.js
@@ -1,5 +1,44 @@
-require('raptor-detect/runtime').detect({
-    'server': function() {
-        require('./img-tag_server');
+var optimizer = require('../');
+var nodePath = require('path');
+var immediateThen = require('raptor-promises/util').immediateThen;
+
+exports.render = function(input, context) {
+    var pageOptimizer = input.optimizer;
+
+    if (!pageOptimizer) {
+        pageOptimizer = optimizer.defaultPageOptimizer;
     }
-});
+    
+    if (!pageOptimizer) {
+        throw new Error('Page optimizer not configured for application. Use require("raptor-optimizer").configureDefault(config) to configure the default page optimizer or provide an optimizer as input using the "optimizer" attribute.');
+    }
+
+    var src = input.src;
+    var imgPath = nodePath.resolve(input.dirname, src);
+
+    context.beginAsyncFragment(function(asyncContext, asyncFragment) {
+        return pageOptimizer.then(function(pageOptimizer) {
+            var optimizerRenderContext = optimizer.getRenderContext(context);
+            var optimizerContext = context.getAttributes().optimizerContext;
+
+            if (!optimizerContext) {
+                optimizerContext = context.getAttributes().optimizerContext = pageOptimizer.createOptimizerContext();
+                optimizerContext.renderContext = context;
+                optimizerContext.enabledExtensions = optimizerRenderContext.getEnabledExtensions();
+            }
+
+            pageOptimizer.resolveResourceUrlCached(
+                    imgPath,
+                    optimizerContext)
+                .then(function(url) {
+                    asyncContext.write('<img src="' + url + '"');        
+                    if (input['*']) {
+                        asyncContext.attrs(input['*']);
+                    }
+                    asyncContext.write('>');
+
+                    asyncFragment.end();
+                });
+        });
+    });
+};
---
+++ taglib/img-tag_browser.js
@@ -0,0 +1,3 @@
+exports.render = function() {
+    throw new Error('Not yet implemented for browser');
+};
