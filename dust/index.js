5ec08ceef9c12031c9c6c48ab7cb4662843022f7
Patrick Steele-Idem <pnidem@gmail.com>
2014-06-16 16:56:06 -0600

    Dust: Support relative path for "packagePath" for @optimizer-page

--- dust/index.js
+++ dust/index.js
@@ -1,8 +1,23 @@
 var raptorDust = require('raptor-dust');
+var nodePath = require('path');
+require('raptor-ecma/es6');
 
 exports.registerHelpers = function(dust) {
     raptorDust.registerHelpers({
-        'optimizer-page': require('../taglib/page-tag'),
+        'optimizer-page': {
+            buildInput: function(chunk, context, bodies, params, renderContext) {
+                var packagePath = params.packagePath;
+                if (!packagePath) {
+                    params.packagePath = nodePath.join(nodePath.dirname(context.templateName), 'optimizer.json');
+                } else if (packagePath.startsWith('./')) {
+                    params.packagePath = nodePath.join(nodePath.dirname(context.templateName), packagePath);
+                }
+                
+                return params;
+
+            },
+            renderer: require('../taglib/page-tag')
+        },
         'optimizer-slot': require('../taglib/slot-tag'),
         'optimizer-head': require('../taglib/head-tag'),
         'optimizer-body': require('../taglib/body-tag'),
