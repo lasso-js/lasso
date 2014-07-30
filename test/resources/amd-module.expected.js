0e0382d15e1fa7669cea8fc9553a905f3d922578
Patrick Steele-Idem <pnidem@gmail.com>
2014-07-30 09:26:37 -0600

    Updating test cases based on changes to raptor-optimizer-require

--- test/resources/amd-module.expected.js
+++ test/resources/amd-module.expected.js
@@ -1,11 +1,3 @@
-$rmod.def("/amd-module", function(require, exports, module, __filename, __dirname) { var foo = require('foo');
-exports.action = function () {
-};
-});
-$rmod.def("/foo@1.0.0/lib/index", function(require, exports, module, __filename, __dirname) { exports.foo = "1.0.0";
-require('baz');
-require('require/baz');
-});
 $rmod.def("/baz@3.0.0/lib/index", function(require, exports, module, __filename, __dirname) { exports.baz = "3.0.0";
 });
 $rmod.main("/baz@3.0.0", "lib/index");
@@ -14,5 +6,13 @@ $rmod.def("/require@4.0.0/baz/index", function(require, exports, module, __filen
 });
 $rmod.main("/require@4.0.0/baz", "index");
 $rmod.dep("", "require", "4.0.0");
+$rmod.def("/foo@1.0.0/lib/index", function(require, exports, module, __filename, __dirname) { exports.foo = "1.0.0";
+require('baz');
+require('require/baz');
+});
 $rmod.main("/foo@1.0.0", "lib/index");
 $rmod.dep("", "foo", "1.0.0");
+$rmod.def("/amd-module", function(require, exports, module, __filename, __dirname) { var foo = require('foo');
+exports.action = function () {
+};
+});
--- test/resources/foo-bar-bundle.expected.js
+++ test/resources/foo-bar-bundle.expected.js
@@ -1,7 +1,3 @@
-$rmod.def("/foo@1.0.0/lib/index", function(require, exports, module, __filename, __dirname) { exports.foo = "1.0.0";
-require('baz');
-require('require/baz');
-});
 $rmod.def("/baz@3.0.0/lib/index", function(require, exports, module, __filename, __dirname) { exports.baz = "3.0.0";
 });
 $rmod.main("/baz@3.0.0", "lib/index");
@@ -10,11 +6,15 @@ $rmod.def("/require@4.0.0/baz/index", function(require, exports, module, __filen
 });
 $rmod.main("/require@4.0.0/baz", "index");
 $rmod.dep("", "require", "4.0.0");
+$rmod.def("/foo@1.0.0/lib/index", function(require, exports, module, __filename, __dirname) { exports.foo = "1.0.0";
+require('baz');
+require('require/baz');
+});
 $rmod.main("/foo@1.0.0", "lib/index");
 $rmod.dep("", "foo", "1.0.0");
+$rmod.dep("/$/bar", "baz", "3.0.0");
 $rmod.def("/bar@2.0.0/lib/bar", function(require, exports, module, __filename, __dirname) { exports.bar = "2.0.0";
 require('baz');
 });
-$rmod.dep("/$/bar", "baz", "3.0.0");
 $rmod.main("/bar@2.0.0", "lib/bar");
 $rmod.dep("", "bar", "2.0.0");
