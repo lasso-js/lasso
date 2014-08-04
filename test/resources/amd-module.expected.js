1ecedc83998f863bb1ec8a926bcc91f877ee0603
Patrick Steele-Idem <pnidem@gmail.com>
2014-08-04 13:52:25 -0600

    Fixed test cases after moving metadata first

--- test/resources/amd-module.expected.js
+++ test/resources/amd-module.expected.js
@@ -1,17 +1,17 @@
-$rmod.def("/baz@3.0.0/lib/index", function(require, exports, module, __filename, __dirname) { exports.baz = "3.0.0";
-});
+$rmod.main("/foo@1.0.0", "lib/index");
+$rmod.dep("", "foo", "1.0.0");
 $rmod.main("/baz@3.0.0", "lib/index");
 $rmod.dep("/$/foo", "baz", "3.0.0");
-$rmod.def("/require@4.0.0/baz/index", function(require, exports, module, __filename, __dirname) { exports.baz = true;
+$rmod.def("/baz@3.0.0/lib/index", function(require, exports, module, __filename, __dirname) { exports.baz = "3.0.0";
 });
 $rmod.main("/require@4.0.0/baz", "index");
 $rmod.dep("", "require", "4.0.0");
+$rmod.def("/require@4.0.0/baz/index", function(require, exports, module, __filename, __dirname) { exports.baz = true;
+});
 $rmod.def("/foo@1.0.0/lib/index", function(require, exports, module, __filename, __dirname) { exports.foo = "1.0.0";
 require('baz');
 require('require/baz');
 });
-$rmod.main("/foo@1.0.0", "lib/index");
-$rmod.dep("", "foo", "1.0.0");
 $rmod.def("/amd-module", function(require, exports, module, __filename, __dirname) { var foo = require('foo');
 exports.action = function () {
 };
