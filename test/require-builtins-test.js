45afcb360b0a103ace73db70b625841b8a170431
Patrick Steele-Idem <pnidem@gmail.com>
2014-10-22 11:18:32 -0600

    Added tests for requiring builtins

---
+++ test/require-builtins-test.js
@@ -0,0 +1,61 @@
+'use strict';
+var chai = require('chai');
+chai.Assertion.includeStack = true;
+require('chai').should();
+var expect = require('chai').expect;
+var path = require('path');
+var util = require('./util');
+var outputDir = path.join(__dirname, 'build');
+
+
+require('app-module-path').addPath(path.join(__dirname, 'src'));
+describe('optimizer/index', function() {
+    beforeEach(function(done) {
+        util.rmdirRecursive(outputDir);
+        for (var k in require.cache) {
+            if (require.cache.hasOwnProperty(k)) {
+                delete require.cache[k];
+            }
+        }
+        require('raptor-promises').enableLongStacks();
+        require('raptor-logging').configureLoggers({
+            'optimizer': 'WARN',
+            'raptor-cache': 'WARN'
+        });
+        done();
+    });
+
+    it('should allow for requiring builtins', function(done) {
+        var optimizer = require('../');
+        var pageOptimizer = optimizer.create({
+            fileWriter: {
+                outputDir: outputDir,
+                urlPrefix: '/',
+                fingerprintsEnabled: false
+            },
+            bundlingEnabled: true,
+            plugins: [
+                {
+                    plugin: 'optimizer-require',
+                    config: {
+                        includeClient: false
+                    }
+                }
+            ]
+        }, __dirname, __filename);
+
+        // var writerTracker = require('./WriterTracker').create(pageOptimizer.writer);
+        pageOptimizer.optimizePage({
+                pageName: 'testPage',
+                dependencies: [
+                    './optimizer.json'
+                ],
+                from: path.join(__dirname, 'test-builtins-project').replace(/\\/g, '/')
+            }, function(err, optimizedPage) {
+                if (err) {
+                    return done(err);
+                }
+                done();
+            });
+    });
+});
---
+++ test/test-builtins-project/main.js
@@ -0,0 +1,8 @@
+require('assert');
+require('buffer');
+require('events');
+require('path');
+require('process');
+require('stream');
+require('util');
+require('raptor-loader');
