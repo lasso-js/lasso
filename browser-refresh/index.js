b1111d3dadb17cee2b1f70823980773119439905
Patrick Steele-Idem <pnidem@gmail.com>
2014-12-11 11:19:24 -0700

    Use browser-refresh-client instead

--- browser-refresh/index.js
+++ browser-refresh/index.js
@@ -1,25 +1,19 @@
+var browserRefreshClient = require('browser-refresh-client');
 var optimizer = require('../');
 
 exports.enable = function(patterns) {
+    if (!browserRefreshClient.isBrowserRefreshEnabled()) {
+        return;
+    }
+
     if (!patterns) {
-        patterns = "*.css *.less *.styl *.scss *.sass *.png *.jpeg *.jpg *.gif *.webp";
+        // Reasonable default with client-side only files...
+        patterns = '*.css *.less *.styl *.scss *.sass *.png *.jpeg *.jpg *.gif *.webp *.svg';
     }
 
-    if (process.env.BROWSER_REFRESH_URL) {
-
-        var modifiedEvent = 'optimizer.fileModified.' + patterns;
-
-        process.send({
-            type: 'browser-refresh.specialReload',
-            patterns: patterns,
-            modifiedEvent: modifiedEvent
+    browserRefreshClient
+        .enableSpecialReload(patterns)
+        .onFileModified(function(path) {
+            optimizer.handleWatchedFileChanged(path);
         });
-
-        process.on('message', function(m) {
-            if (typeof m === 'object' && m.type === modifiedEvent) {
-                optimizer.handleWatchedFileChanged(m.path);
-            }
-        });
-
-    }
 };
