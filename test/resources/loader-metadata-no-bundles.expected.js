c736d4d58a0d91ebf81297e0f0f3455b598d53d4
Patrick Steele-Idem <pnidem@gmail.com>
2015-01-21 16:48:39 -0700

    Fixed name of async bundles

--- test/resources/loader-metadata-no-bundles.expected.js
+++ test/resources/loader-metadata-no-bundles.expected.js
@@ -1,6 +1,6 @@
 (function(loaderMeta) {
 loaderMeta["package1"] = {"js":["/static/test-async-project/async1/async1-module.js"]};
 loaderMeta["package2"] = {"js":["/static/test-async-project/async2/async2-module.js"]};
-loaderMeta["package1-a"] = {"js":["/static/raptor-modules-meta-testPage.js","/static/test-async-project/async1/async1-module.js"]};
+loaderMeta["package1-a"] = {"js":["/static/raptor-modules-meta-testPage-async.js","/static/test-async-project/async1/async1-module.js"]};
 loaderMeta["main"] = {};
 })(window.$rloaderMeta || (window.$rloaderMeta = {}));
