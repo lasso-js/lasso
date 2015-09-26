b0af778d18ea195b6b0a6a174d510e5d1808e9fc
Patrick Steele-Idem <pnidem@gmail.com>
2015-09-26 09:52:19 -0600

    Builtin es6 support for .es6 files

---
+++ test/es6-test.js
@@ -0,0 +1,74 @@
+'use strict';
+var chai = require('chai');
+chai.Assertion.includeStack = true;
+require('chai').should();
+var expect = require('chai').expect;
+var path = require('path');
+var outputDir = path.join(__dirname, 'build');
+
+require('app-module-path').addPath(path.join(__dirname, 'src'));
+
+require('babel/register')({
+    extensions: [".es6"]
+});
+
+describe('lasso/index', function() {
+    it('should allow for es6 module transpiling using babel', function(done) {
+        var lasso = require('../');
+        var theLasso = lasso.create({
+            outputDir: outputDir,
+            urlPrefix: '/',
+            fingerprintsEnabled: false,
+            bundlingEnabled: false
+        }, __dirname, __filename);
+
+        var writerTracker = require('./WriterTracker').create(theLasso.writer);
+        theLasso.lassoPage({
+                pageName: 'testPage',
+                cache: false,
+                dependencies: [
+                    {
+                        type: 'require',
+                        path: path.join(__dirname, './fixtures/babel/test.es6')
+                    }
+                ]
+            })
+            .then(function(lassoPageResult) {
+                var testCode = writerTracker.getCodeForFilename('test.es6.js');
+                expect(testCode).to.contain('require("./foo")');
+
+                var fooCode = writerTracker.getCodeForFilename('foo.es6.js');
+                expect(fooCode).to.contain('123');
+
+                lasso.flushAllCaches(done);
+            })
+            .done();
+    });
+
+    it('should allow for vanilla es6 transpiling using babel', function(done) {
+        var lasso = require('../');
+        var theLasso = lasso.create({
+            outputDir: outputDir,
+            urlPrefix: '/',
+            fingerprintsEnabled: false,
+            bundlingEnabled: false
+        }, __dirname, __filename);
+
+        var writerTracker = require('./WriterTracker').create(theLasso.writer);
+        theLasso.lassoPage({
+                pageName: 'testPage',
+                cache: false,
+                dependencies: [
+                    path.join(__dirname, './fixtures/babel/vanilla.es6')
+                ]
+            })
+            .then(function(lassoPageResult) {
+                var testCode = writerTracker.getCodeForFilename('vanilla.es6.js');
+                expect(testCode).to.not.contain('let');
+                expect(testCode).to.not.contain('require');
+
+                lasso.flushAllCaches(done);
+            })
+            .done();
+    });
+});
---
+++ test/fixtures/babel/test.es6
@@ -0,0 +1,13 @@
+import foo from "./foo";
+
+if (true) {
+  let a = 1;
+} else {
+  let a = 1;
+}
+
+class Foo {
+  constructor(a) {
+    this.a = a;
+  }
+}
