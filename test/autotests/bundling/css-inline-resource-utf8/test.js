1fad53d101ff6c122b789a9d7bf48c1fc899ae91
Patrick Steele-Idem <pnidem@gmail.com>
2016-05-12 15:14:19 -0600

    Fixes #141 - Add support for UTF8 encoding inline images in CSS files

---
+++ test/autotests/bundling/css-inline-resource-utf8/test.js
@@ -0,0 +1,29 @@
+var expect = require('chai').expect;
+var path = require('path');
+
+exports.getLassoConfig = function() {
+    return {
+        fingerprintsEnabled: false,
+        includeSlotNames: true
+    };
+};
+
+exports.getInputs = function() {
+    return [
+        {
+            lassoOptions: {
+                dependencies: [
+                    path.join(__dirname, 'foo.css')
+                ]
+            },
+            check(lassoPageResult, writerTracker, helpers) {
+                expect(writerTracker.getOutputFilenames()).to.deep.equal([
+                        'bundling-css-inline-resource-utf8-head.css'
+                    ]);
+
+                var actualCSS = writerTracker.getCodeForFilename('bundling-css-inline-resource-utf8-head.css');
+                helpers.compare(actualCSS, '.css');
+            }
+        }
+    ];
+};
