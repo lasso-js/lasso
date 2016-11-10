73c91101172fe561c71a98eb4e325e392712d3be
Patrick Steele-Idem <pnidem@gmail.com>
2016-11-10 16:58:35 -0700

    Added failing test for #178 - Cache key changes when lasso is reconfigured even if config did not change

---
+++ test/autotests/api/config-fingerprint/test.js
@@ -0,0 +1,22 @@
+
+var expect = require('chai').expect;
+
+exports.check = function(lasso, helpers, done) {
+
+    var config = {
+        bundles: [
+            {
+                name: 'foo',
+                dependencies: [
+                    "require: " + require.resolve('./foo.js')
+                ]
+            }
+        ]
+    };
+
+    var myLasso1 = lasso.create(config);
+    var myLasso2 = lasso.create(config);
+    expect(myLasso1.config.getConfigFingerprint()).to.equal(myLasso2.config.getConfigFingerprint());
+    done();
+};
+
