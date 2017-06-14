9326574d21eebd799cf79fec6e39a22022758b81
Michael Rawlings <ml.rawlings@gmail.com>
2017-06-14 03:04:35 -0700

    remove babel support from core

--- test/autotests/modules-es6/es6-mask-define/simple.es6
+++
@@ -1,4 +0,0 @@
-window.foo = function() {
-    const foo = '123';
-    return foo;
-};
--- test/autotests/modules-es6/not-whitelisted/main.js
+++
@@ -1,5 +0,0 @@
-exports.filename = __filename;
-
-exports.notWhitelisted = require('./not-whitelisted');
-
-window.main = module.exports;
