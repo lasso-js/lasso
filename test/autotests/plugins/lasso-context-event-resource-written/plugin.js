9a8013eb283d96b56334fa7a635c5b09d4e0805c
Austin Kelleher <austin.kell47@gmail.com>
2017-08-03 13:15:56 -0400

    Fixes #206 - Emit bundleWritten and resourceWritten events from the LassoContext. (#213)

---
+++ test/autotests/plugins/lasso-context-event-resource-written/plugin.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var events = [];
+
+module.exports = exports = function(lasso, config) {
+    lasso.on('beforeBuildPage', (event) => {
+        var context = event.context;
+
+        context.on('resourceWritten', event => {
+            const correctSourceFile = event.sourceFile.endsWith('/test/autotests/plugins/lasso-context-event-resource-written/fonts/Aleo-Regular.woff');
+            const correctOutputFile = event.outputFile.endsWith('/test/build/plugins-lasso-context-event-resource-written/plugins-lasso-context-event-resource-written/autotest$0.0.0/fonts/Aleo-Regular.woff');
+
+            events.push({
+                url: event.url,
+                sourceFile: correctSourceFile,
+                outputFile: correctOutputFile
+            });
+        });
+    });
+};
+
+exports.events = events;
