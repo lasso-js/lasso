dc7a172e620944a7a803ee0ce93e5e5d56dc53b1
Scott Davis <scodavis@ebay.com>
2018-11-27 17:01:19 -0500

    wrap code in listener if inline slot is defer

--- src/Slot.js
+++ src/Slot.js
@@ -30,13 +30,17 @@ Slot.prototype = {
         });
     },
 
+    wrapInDocumentLoaded: function(code) {
+        return '(function() { var run = function() { ' + code + ' }; if (document.readyState === "loading") { document.addEventListener("DOMContentLoaded", run); } else { run(); } })();';
+    },
+
     buildHtml: function() {
         var output = [];
         for (var i = 0, len = this.content.length; i < len; i++) {
             var content = this.content[i];
             if (content.inline) {
                 if (this.contentType === 'js') {
-                    output.push('<script ...data.inlineScriptAttrs marko-body="static-text">' + content.code + '</script>'); // eslint-disable-line no-template-curly-in-string
+                    output.push('<script ...data.inlineScriptAttrs marko-body="static-text"><if(data.inlineScriptAttrs.defer)>' + this.wrapInDocumentLoaded(content.code) + '</if><else>' + content.code + '</else></script>'); // eslint-disable-line no-template-curly-in-string
                 } else if (this.contentType === 'css') {
                     output.push('<style ...data.inlineStyleAttrs marko-body="static-text">' + content.code + '</style>'); // eslint-disable-line no-template-curly-in-string
                 }
